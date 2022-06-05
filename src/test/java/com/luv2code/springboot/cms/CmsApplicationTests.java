cb652cf9160d4ea6006819e50472854b630ea11a
DeepakGuptaIT <deepakguptaoptimistic@gmail.com>
2022-06-05 08:52:39 +0530

    First commit

---
+++ src/test/java/com/luv2code/springboot/cms/CmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luv2code.springboot.cms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
